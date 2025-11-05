e37ac231b095a4ef2a4f8aebf80a2130092347d3
lai2-24 <lai2-24@itismeucci.com>
2025-11-05 11:48:09 +0100

    creata socket e giocatori

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,36 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+
+        ServerSocket mioServerSocket = new ServerSocket(3000);
+
+        Socket G1 = mioServerSocket.accept();
+
+        BufferedReader G1_in = new BufferedReader(new InputStreamReader(G1.getInputStream()));
+        PrintWriter G1_out = new PrintWriter(G1.getOutputStream(), true);
+        
+        System.out.println("Giocatore 1 collegato");
+        G1_out.println("WAIT");
+
+        Socket G2 = mioServerSocket.accept();
+
+        BufferedReader G2_in = new BufferedReader(new InputStreamReader(G2.getInputStream()));
+        PrintWriter G2_out = new PrintWriter(G2.getOutputStream(), true);
+        
+        System.out.println("Giocatore 2 collegato");
+        G1_out.println("READY");
+        G2_out.println("READY");
+
+        
+       
     }
 }
